a2b4a4d59a26dbe32e92a4e36040cf8bdf4b1648
user <vika@mil.ru>
2020-12-22 14:37:25 +0300

    aside_03

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -207,7 +207,114 @@ menu__list.onclick = function () {
     back.classList.remove('lock');
 }
 
+///////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
+let menuParents = document.querySelectorAll('.menu-page__parent');
+
+for (let index = 0; index < menuParents.length; index++) {
+    const menuParent = menuParents[index];
+    menuParent.addEventListener("mouseenter", function (e) {
+        menuParent.classList.add("_active");
+    });
+    menuParent.addEventListener("mouseleave", function (e) {
+        menuParent.classList.remove("_active");
+    });
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+let _slideUp = (target, duration = 500) => {
+        // debugger
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = height //= target.offsetHeight + 'px';
+        target.offsetHeight;
+        // target.style.overflow = 'hidden';
+        target.style.height = 0;
+        // target.style.paddingTop = 0;
+        // target.style.paddingBottom = 0;
+    window.setTimeout(() => {
+        target.style.display = 'none';
+        target.style.removeProperty('height');
+        // target.style.removeProperty('padding-top');
+        // target.style.removeProperty('padding-bottom');
+        // target.style.removeProperty('margin-top');
+        // target.style.removeProperty('margin-bottom');
+        // target.style.removeProperty('overflow');
+        // target.style.removeProperty('transition-duration');
+        // target.style.removeProperty('transition-property');
+        target.classList.remove('_slide');
+    }, duration);
+}
+let _slideDown = (target, duration = 500) => {
+    // debugger
+    // target.style.removeProperty('display');
+    // let display = window.getComputedStyle(target).display;
+    // if (display === 'none')
+    // let    display = 'block';
+    // target.style.visibility = "hidden";
+    target.style.display = 'block';
+    height =  target.offsetHeight + 'px';
+    // target.style.visibility = "visible";
+    // debugger
+    // target.style.overflow = 'hidden';
+
+    target.style.height = 0;
+    // target.style.paddingTop = 0;
+    // target.style.paddingBottom = 0;
+    // target.style.marginTop = 0;
+    // target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height;
+    // target.offsetHeight;
+    //window.getComputedStyle(target)
+    // target.style.removeProperty('padding-top');
+    // target.style.removeProperty('padding-bottom');
+    // target.style.removeProperty('margin-top');
+    // target.style.removeProperty('margin-bottom');
+    window.setTimeout(() => {
+        target.style.removeProperty('height');
+        // target.style.removeProperty('overflow');
+        // target.style.removeProperty('transition-duration');
+        // target.style.removeProperty('transition-property');
+        target.classList.remove('_slide');
+    }, duration);
+}
+
+let open = false;
+let height = 0;
+let _slideToggle = (target, duration = 500) => {
+    if (!target.classList.contains('_slide')) {
+        target.classList.add('_slide');
+        // let height = target.style.height;
+        // debugger
+        if (!open) {
+            open = !open;
+            return _slideDown(target, duration);
+        } else {
+            open = !open;
+            return _slideUp(target, duration);
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+let menuPageBurger = document.querySelector('.menu-page__burger');
+let menuPageBody = document.querySelector('.menu-page__body');
+
+menuPageBurger.addEventListener("click", function (e) {
+    menuPageBurger.classList.toggle('_active');
+    _slideToggle(menuPageBody);
+});
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
 // let isMobile = {
 //     Android: function () {
 //         return navigator.userAgent.match(/Android/i);
