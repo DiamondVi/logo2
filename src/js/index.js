9edd770ac7d2157aaef9938a7a4ac9ff6a09705e
User <2176589@mail.ru>
2020-12-27 14:50:34 -0800

    select

--- src/js/index.js
+++ src/js/index.js
@@ -136,6 +136,28 @@ searchSelect.addEventListener('click', function (e) {
 
 /////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
+
+let checkboxCategories = document.querySelectorAll('.categories-search__checkbox');
+
+for (let index = 0; index < checkboxCategories.length; index++){
+    const checkboxCategory = checkboxCategories[index];
+    checkboxCategory.addEventListener("change", function (e){
+        checkboxCategory.classList.toggle('_active');
+
+        let checkboxActiveCategories = document.querySelectorAll('.categories-search__checkbox._active');
+
+        if (checkboxActiveCategories.length > 0){
+            searchSelect.classList.add('_categories');
+            let searchQuantity = searchSelect.querySelector('.search-page__quantity');
+            searchQuantity.innerHTML = searchQuantity.getAttribute('data-text') + " " + checkboxActiveCategories.length;
+        } else {
+            searchSelect.classList.remove('_categories');
+        }
+    });
+}
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
 // let isMobile = {
 //     Android: function () {
 //         return navigator.userAgent.match(/Android/i);
